69502a5bfe3d1d29ca96c33438885a627a0cb5da
DiprodipDas <diprodip755@gmail.com>
2023-02-07 22:18:10 +0600

    primary requirement complete

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
 const app = express();
@@ -15,11 +15,36 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ntptfwh.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        const courseCollection = client.db('eduMart').collection('courses');
+
+
+        // app.get('/courses',async(req,res)=>{
+        //     res.send('hi')
+        // })
+
+        app.get('/courses', async (req, res) => {
+            const query = {}
+            const cursor = courseCollection.find(query);
+            const courses = await cursor.toArray();
+            res.send(courses);
+        });
+
+        app.get('/courses/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const course= await courseCollection.findOne(query);
+            res.send(course);
+        });
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.error(err));
 
 
 
